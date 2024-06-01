7f92276de6c2c1409a578acf145f7cd78d1ddcab
Dali Wang <wangd@ornl.gov>
2024-06-01 18:41:06 -0400

    add args to the code

--- NADaymet_subdomain.py
+++ NADaymet_subdomain.py
@@ -1,7 +1,9 @@
+
 # create NADaymet_subdomain
 
 import netCDF4 as nc
 import numpy as np
+import os,sys
 
 from datetime import datetime
 
@@ -10,35 +12,57 @@ current_date = datetime.now()
 # Format date to mmddyyyy
 formatted_date = current_date.strftime('%y%m%d')
 
-AOI='NADaymet1M'
+def main():
 
-# save to the 1D domain file
-AOI_gridID = str(AOI)+'_gridID.c'+ formatted_date + '.nc'
-AOIgridID_start = 0
-AOIgridID_end = 1000000
-dst = nc.Dataset(AOI_gridID, 'w', format='NETCDF4')
+    args = sys.argv[1:]
+    # Check the number of arguments
+    if len(sys.argv) != 4  or sys.argv[1] == '--help':  # sys.argv includes the script name as the first argument
+        print("Example use: python NADaymet_subdomain.py <input_path> <output_path> <AOI_name>")
+        print(" <input_path>: path to the 1D NA domain directory")
+        print(" <output_path>:  path to save the grid1D for AOI domain")
+        print(" <AOI_name>:  case name")
+        print(" Uses NA domain (domain.lnd.Daymet_NA.1km.1d.c240521.nc) to creat <AOI>_gridID.nc for AOI domain")      
+        exit(0)
 
-source_file = 'domain.lnd.Daymet_NA.1km.1d.c240521.nc'
-# open the 1D domain data
-src = nc.Dataset(source_file, 'r', format='NETCDF4')
 
-# read gridID
-NA_gridID = src.variables['gridID'][:]
+    domain_path = args[0]
+    output_path = args[1]
+    AOI = args[2]
 
-AOI_gridID_arr = NA_gridID[0][AOIgridID_start:AOIgridID_end].copy()
 
-print(AOI_gridID_arr.shape)
+    print(len(sys.argv), domain_path,output_path, AOI)
+    
 
-# create the gridIDs, lon, and lat variable
-ni_dim = dst.createDimension('ni', AOI_gridID_arr.size)
-nj_dim = dst.createDimension('nj', 1)
+    # save to the 1D domain file
+    AOI_gridID = output_path +'/'+str(AOI)+'_gridID.c'+ formatted_date + '.nc'
+    AOIgridID_start = 0
+    AOIgridID_end = 1000000
+    dst = nc.Dataset(AOI_gridID, 'w', format='NETCDF4')
 
-gridID_var = dst.createVariable('gridID', np.int32, ('nj','ni'), zlib=True, complevel=5)
-gridID_var.long_name = 'gridId in the NA domain'
-gridID_var.decription = "start from #0 at the upper left corner of the domain, covering all land and ocean gridcells" 
-dst.variables['gridID'][...] = AOI_gridID_arr
-dst.title = 'First 1M land gridcells in the NA Daymet domain'
+    source_file = domain_path +'/domain.lnd.Daymet_NA.1km.1d.c240521.nc'
+    # open the 1D domain data
+    src = nc.Dataset(source_file, 'r', format='NETCDF4')
 
-src.close()
-dst.close()
+    # read gridID
+    NA_gridID = src.variables['gridID'][:]
 
+    AOI_gridID_arr = NA_gridID[0][AOIgridID_start:AOIgridID_end].copy()
+
+    print(AOI_gridID_arr.shape)
+
+    # create the gridIDs, lon, and lat variable
+    ni_dim = dst.createDimension('ni', AOI_gridID_arr.size)
+    nj_dim = dst.createDimension('nj', 1)
+
+    gridID_var = dst.createVariable('gridID', np.int32, ('nj','ni'), zlib=True, complevel=5)
+    gridID_var.long_name = 'gridId in the NA domain'
+    gridID_var.decription = "start from #0 at the upper left corner of the domain, covering all land and ocean gridcells" 
+    dst.variables['gridID'][...] = AOI_gridID_arr
+    dst.title = 'First 1M land gridcells in the NA Daymet domain'
+
+    src.close()
+    dst.close()
+
+
+if __name__ == '__main__':
+    main()
